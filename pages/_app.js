7fe82cc6582e9c2f92dc5256f91e2efbbe8d9215
xRestv <anabel3231992@gmail.com>
2024-09-29 13:00:10 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,18 @@
-/** none
+import "../styles/globals.css";
+import MainLayout from "../layout/mainLayout";
+import { useRouter } from "next/router";
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  return (
+    <MainLayout>
+      {/* Add your random text here */}
+      <div>
+        <p>This is some random text displayed on the page!</p>
+      </div>
+      <Component {...pageProps} />
+    </MainLayout>
+  );
+}
+
+export default MyApp;
